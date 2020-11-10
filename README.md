9f4791887b72ebeb2bb7f874e22f49d8a67c6243
chaiinchomp <chaiinchomp@users.noreply.github.com>
2020-11-09 19:42:54 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# os-league-tools-api
+ Backend API for use with OS League Tools webapp
---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const hiscores = require("osrs-json-hiscores");
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.get("/hiscores/:rsn", (req, res) => {
+  hiscores
+    .getStatsByGamemode(req.params.rsn, "seasonal")
+    .then(response => res.send(response))
+    .catch(err => {
+      res.status(404).send({ status: 404, error: err });
+    });
+});
+
+app.listen(8080, () => console.log(`OS League Tools API listening on port ${8080}!`));
